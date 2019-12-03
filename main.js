a9d6f041d41403ca18a7d360abf66fcf60ac5a76
fedPass <federica.passanante@gmail.com>
2019-12-03 09:15:59 +0100

    prove su input element per invio messaggio

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ $(document).ready(function(){
     // for (var i = 0; i < 5; i++) {
     //     $('.contactPreview').clone().appendTo('.contactList');
     // }
+    $('#messageInput').click(function(){
+        $('#messageInput').val('');
+    })
+
     $('#sendMess').click(function(){
         var messaggio = $('#messageInput').val();
         console.log(messaggio);
-
-        $(messaggio).append('<div class="message received"><span>' + messaggio + '</span><span>16:45</span></div>');
+        $(messaggio).append('<div class="message sent"><span>' + messaggio + '</span><span>16:45</span></div>');
+        $('#messageInput').val('');
     })
 
 })
