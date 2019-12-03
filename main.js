bd7287b30171292f9d61dd4391bb5733e8fab4d7
fedPass <federica.passanante@gmail.com>
2019-12-03 18:48:45 +0100

    ricerca barra utenti da completare

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
         }
     });
     //quando sto digitando cambia l'icona
-    $('#messageInput').keypress(function(event){
+    $('#messageInput').keyup(function(event){
         var textMessage = $('#messageInput').val();
         //se textMessage.length è maggiore di 0 rimuovi microfono e inserisci aeroplanino
         if (textMessage.length != 0) {
@@ -30,38 +30,52 @@ $(document).ready(function(){
     $('#searchBar i').click(function(){
         var nomeRicercato = $('#searchBar input').val();
         console.log(nomeRicercato);
+        if (nomeRicercato.length > 0) {
+            //prendi ogni nome tra i contatti
+            $('.messagePreview .name').each(function(){
+                //mettilo in una variabile
+                var nomeContatto = $(this).text();
+                for (var i = 0; i < nomeRicercato.length; i++) {
+                    if (nomeContatto[i].toUpperCase() != nomeRicercato[i].toUpperCase()) {
+                        //così mi nasconde solo lo span
+                        $(this).hide();
+                    }
+                }
+            });
+        } else {
+            //se l'input è vuoto mostrameli tutti
+            $('messagePreview').show();
+        }
     });
+});
 
+function sendAnswer() {
+    //devo clonare il template
+    var newMessage = $('.template .message').clone();
+    //inserire il nuovomessaggio nel template messagio
+    newMessage.children('.message-text').text('ok!');
+    //aggiungere la classe sent al messaggio
+    newMessage.addClass('received');
+    //aggiungere per visualizzare il messaggio nel containerMessaggi
+    $('.viewMessage').append(newMessage);
+}
 
-
-    function sendAnswer() {
+function sendMessage() {
+    //recupero il value inserito
+    var textMessage = $('#messageInput').val();
+    console.log(textMessage);
+    //devo verificare se ha digitato qualcosa o no
+    if (textMessage.length != 0) {
         //devo clonare il template
         var newMessage = $('.template .message').clone();
         //inserire il nuovomessaggio nel template messagio
-        newMessage.children('.message-text').text('ok!');
+        newMessage.children('.message-text').text(textMessage);
         //aggiungere la classe sent al messaggio
-        newMessage.addClass('received');
+        newMessage.addClass('sent');
         //aggiungere per visualizzare il messaggio nel containerMessaggi
         $('.viewMessage').append(newMessage);
+        //resetto il value dell'input
+        $('#messageInput').val('');
+        //reimposta icona microfono
     }
-
-    function sendMessage() {
-        //recupero il value inserito
-        var textMessage = $('#messageInput').val();
-        console.log(textMessage);
-        //devo verificare se ha digitato qualcosa o no
-        if (textMessage.length != 0) {
-            //devo clonare il template
-            var newMessage = $('.template .message').clone();
-            //inserire il nuovomessaggio nel template messagio
-            newMessage.children('.message-text').text(textMessage);
-            //aggiungere la classe sent al messaggio
-            newMessage.addClass('sent');
-            //aggiungere per visualizzare il messaggio nel containerMessaggi
-            $('.viewMessage').append(newMessage);
-            //resetto il value dell'input
-            $('#messageInput').val('');
-            //reimposta icona microfono
-        }
-    }
-});
+}
