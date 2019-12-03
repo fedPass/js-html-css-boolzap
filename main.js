e8b9f979d4a39427b6b669a0814726ccfa77cd1b
fedPass <federica.passanante@gmail.com>
2019-12-03 15:59:50 +0100

    inserito div template, attivato invio messaggio tramite click

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
         //devo verificare se ha digitato qualcosa o no
         if (textMessage.length != 0) {
             //devo clonare il template
-
+            var newMessage = $('.template .message').clone();
             //inserire il nuovomessaggio nel template messagio
-
+            newMessage.children('.message-text').text(textMessage);
             //aggiungere la classe sent al messaggio
-
+            newMessage.addClass('sent');
             //aggiungere per visualizzare il messaggio nel containerMessaggi
-
+            $('.viewMessage').append(newMessage);
             //resetto il value dell'input
             $('#messageInput').val('');
         }
